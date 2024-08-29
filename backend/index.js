13ab904ea7a8bdf157dec3141bd4417e98611b48
K RAJA <cse.raja007@gmail.com>
2024-08-29 21:14:17 +0530

    depolyment

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
+dotenv.config({});
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+console.log("before db");
+
+// app.listen(PORT, () => {
+//   console.log("before db");
+//   connectDB();
+//   console.log(`Server running at port ${PORT}`);
+// });
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(`Failed to start server: ${err.message}`);
+    process.exit(1);
+  }
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
