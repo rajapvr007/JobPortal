25b92a05db2a32a686cb75e8ccbf1abb8edae1f1
K RAJA <cse.raja007@gmail.com>
2024-08-29 21:40:20 +0530

    depolyment3

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -3,8 +3,11 @@
 // export const APPLICATION_API_END_POINT="https://jobportal-backend-n0t9.onrender.com/api/v1/application";
 // export const COMPANY_API_END_POINT="https://jobportal-backend-n0t9.onrender.com/api/v1/company";
 
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://jobportal-twkg.onrender.com/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://jobportal-twkg.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+  "https://jobportal-twkg.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://jobportal-twkg.onrender.com/api/v1/company";
